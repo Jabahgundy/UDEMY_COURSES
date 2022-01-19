de98d15f84019f6c990510b75d885947897cbef2
Jabah Gundy <gundy157@outlook.com>
2022-01-19 02:26:25 -0500

    updated

---
+++ JavaScript_Course/Functions/passingArgu.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const bookings = [];
+
+const createBooking = function name(
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  //   ES5;
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+const flight = "LH234";
+const jonas = {
+  name: "Jonas Schmedtmann",
+  passport: 2473,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH999";
+  passenger.name = "Mr. " + passenger.name;
+
+  if (passenger.passport === 2473) {
+    alert("Checked in");
+  } else {
+    alert("Wrong passport!");
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
